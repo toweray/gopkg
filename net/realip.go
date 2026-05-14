ca86632127d092e8f6a7c76b27b8cc42507c1506
voittaneena <jastxel@gmail.com>
2026-05-14 14:27:48 +0300

    fix(realip): update RealIP function

--- net/realip.go
+++ net/realip.go
@@ -1,15 +1,19 @@
 package net
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 // RealIP returns the real client IP from the request.
 // It checks Cloudflare and proxy headers before falling back to the remote address.
 func RealIP(c fiber.Ctx) string {
-	if ip := c.Get("CF-Connecting-IP"); ip != "" {
+	if ip := c.Get("X-Real-IP"); ip != "" {
 		return ip
 	}
 	if ip := c.Get("X-Forwarded-For"); ip != "" {
-		return ip
+		return strings.SplitN(ip, ",", 2)[0]
 	}
 	return c.IP()
 }
